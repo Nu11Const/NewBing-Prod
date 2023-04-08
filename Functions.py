fe4318e8bb55c1b8b12e58eadba7643df573a540
VMTask <congmingfan@outlook.com>
2023-04-08 15:32:47 +0800

    update

---
+++ Functions.py
@@ -0,0 +1,72 @@
+import asyncio
+from fastapi import FastAPI, Request, WebSocket
+from fastapi.responses import HTMLResponse
+from EdgeGPT import Chatbot, ConversationStyle
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+
+import json
+app = FastAPI()
+bot_list = []
+app.mount("/assets", StaticFiles(directory="assets"), name="assets")
+
+# 创建Jinja2模板实例
+templates = Jinja2Templates(directory="templates")
+@app.get("/")
+def index(request: Request):
+    # 渲染index.html模板，传入request参数
+    return templates.TemplateResponse("index.html", {"request": request})
+
+@app.websocket("/api/ws")
+async def websocket_endpoint(websocket: WebSocket):
+    await websocket.accept()
+    while True:
+        data_raw = await websocket.receive_text()
+        data = json.loads(data_raw)
+        print(data)
+        id = data["id"]
+        if (bot_list[id]["style"] == "balanced"):
+            style = ConversationStyle.balanced
+        elif(bot_list[id]["style"] == "creative"):
+            style = ConversationStyle.creative
+        elif(bot_list[id]["style"] == "precise"):
+            style = ConversationStyle.precise
+        response = await bot_list[id]["bot"].ask(prompt=data["message"],conversation_style=style)
+        if(len(response["item"]["messages"]) == 0):
+            await websocket.send_text("被Bing拦截")
+        else:
+            await websocket.send_text(response["item"]["messages"][1]["adaptiveCards"][0]["body"][0]["text"])
+        
+        
+        
+@app.post("/api/newchat")
+async def newchat(jsonData: dict):
+    id = len(bot_list)
+    print(jsonData)
+    bot_list.append({
+        "id": id,
+        "style": jsonData["style"],
+        "bot": None,
+        "cookie": jsonData["cookie"]
+    })
+    if not(jsonData["cookie"] == str):
+        temp = jsonData["cookie"]
+        jsonData["cookie"] = json.loads(temp)
+    bot_list[id]["bot"] = Chatbot(cookies=jsonData["cookie"])
+    print(jsonData["cookie"])
+    return bot_list[id]
+
+@app.get("/api/get")
+async def get(jsonData: dict):
+    id = jsonData["id"]
+    return bot_list[id]
+
+@app.post("/api/change_style")
+async def change_style(jsonData:dict):
+    response = {"message": "successful","code":"200"}
+    try:
+        bot_list[jsonData["id"]]["style"] = jsonData["style"]
+    except Exception as err:
+        response["message"] = err
+        response["code"] = "500"
+    return response
